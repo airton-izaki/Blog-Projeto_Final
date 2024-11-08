316413c84651640ad33fe0015be10fc11eb053ce
Pedro Marcato <pedro.marcato@iesb.edu.br>
2024-11-08 10:37:18 -0300

    Pedro Marcato, 08/11/2024

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,33 @@
 import React, { useState } from 'react';
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css';
 import Header from './components/Header/Header';
-import AppRoute from './components/Route/AppRoute';
 import Footer from './components/Footer/Footer';
 import Login from './pages/Login/Login';
-import Home from './pages/Home/Home'; // Importe o componente Home
+import Home from './pages/Home/Home';
+import About from './pages/About/About';
 
 const App = () => {
   const [isAuthenticated, setIsAuthenticated] = useState(false);
-  const [redirectToPostForm, setRedirectToPostForm] = useState(false);
 
   const handleLogin = (username, password) => {
     if (username === 'admin' && password === '123') {
       setIsAuthenticated(true);
-      setRedirectToPostForm(true); // Ativa o redirecionamento    
     } else {
       alert('Digite seu nome ou senha corretamente');
     }
   };
 
-  return (    
+  return (
     <BrowserRouter>
-      <div className="App">        
-        <Header /> 
+      <div className="App">
+        <Header />
 
         <Routes>
-          {/* Rota para exibir Home primeiro */}
           <Route path="/" element={<Home />} />
-
-          {/* Renderiza as rotas protegidas ou a página de login */}
+          <Route path="/about" element={<About />} />
           {isAuthenticated ? (
-            <Route path="*" element={<AppRoute isAuthenticated={isAuthenticated} />} />
+            <Route path="*" element={<div>Authenticated Route</div>} />
           ) : (
             <Route path="*" element={<Login onLogin={handleLogin} />} />
           )}
@@ -39,7 +35,7 @@ const App = () => {
 
         <Footer />
       </div>
-    </BrowserRouter>    
+    </BrowserRouter>
   );
 };
 
